502508e21f3b787f4509b8d3f158ea72c172c21a
Abdi Adem <abdiadem954@gmail.com>
2022-02-18 15:55:58 +0300

    feat: added the function to print the result

--- main.py
+++ main.py
@@ -25,7 +25,23 @@ def random_word_picker():
 
 
 def draw_result(validation_status, user_guess):
-  ...
+  '''
+  A function to print the resutlt in a stylish way
+  '''
+
+  status_grid = ''
+
+  for idx, status in enumerate(validation_status):
+    if status == 1:
+      status_grid += f'{typer.style(" "+user_guess[idx]+" ", fg=typer.colors.WHITE, bg=typer.colors.GREEN, bold=True)} '
+    elif status == 0:
+      status_grid += f'{typer.style(" "+user_guess[idx]+" ", fg=typer.colors.WHITE, bg=typer.colors.YELLOW, bold=True)} '
+    else:
+      status_grid += f'{typer.style(" "+user_guess[idx]+" ", fg=typer.colors.BLACK, bg=typer.colors.WHITE, bold=True)} '
+
+  status_grid += '\n'
+
+  return status_grid
 
 
 def validate(random_word, user_guess):
